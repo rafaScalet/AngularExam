4f3c5c3517cc8d16d23a209e39aafd4fabbb0bcb
rafaScalet <rafaelscalet2018@gmail.com>
2024-06-03 21:04:52 -0300

    Refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { BooksComponent } from './books/books.component';
-import { AuthorsComponent } from './authors/authors.component';
+import { HomeComponent } from './components/home/home.component';
+import { BooksComponent } from './components/books/books.component';
+import { AuthorsComponent } from './components/authors/authors.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
