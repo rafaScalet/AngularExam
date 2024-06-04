18d4ea511f9df3e40b88f0082d5207e3fdc62e7b
rafaScalet <rafaelscalet2018@gmail.com>
2024-06-04 00:03:56 -0300

    delete and update func

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -9,6 +9,7 @@ import { BookService } from '../../services/book.service';
   styleUrl: './books.component.css',
 })
 export class BooksComponent implements OnInit {
+  isEditing: boolean = false;
   books: Book[] = [];
   bookFormGroup: FormGroup;
 
@@ -24,17 +25,42 @@ export class BooksComponent implements OnInit {
     });
   }
   ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
     this.service.getBooks().subscribe({
       next: (data) => (this.books = data),
     });
   }
 
   save() {
-    this.service.postBook(this.bookFormGroup.value).subscribe({
-      next: (data) => {
-        this.books.push(data);
-        this.bookFormGroup.reset();
-      },
+    if (this.isEditing) {
+      this.service.putBook(this.bookFormGroup.value).subscribe({
+        next: () => {
+          this.loadBooks();
+          this.isEditing = false;
+          this.bookFormGroup.reset();
+        },
+      });
+    } else {
+      this.service.postBook(this.bookFormGroup.value).subscribe({
+        next: (data) => {
+          this.books.push(data);
+          this.bookFormGroup.reset();
+        },
+      });
+    }
+  }
+
+  delete(book: Book) {
+    this.service.deleteBook(book).subscribe({
+      next: () => this.loadBooks(),
     });
+  }
+
+  update(book: Book) {
+    this.isEditing = true;
+    this.bookFormGroup.setValue(book);
   }
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -15,7 +15,15 @@ export class BookService {
     return this.http.get<Book[]>(this.url);
   }
 
-  postBook(book: Book): Observable<Book>{
+  postBook(book: Book): Observable<Book> {
     return this.http.post<Book>(this.url, book);
+  }
+
+  deleteBook(book: Book): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${book.id}`);
+  }
+
+  putBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(`${this.url}/${book.id}`, book);
   }
 }
