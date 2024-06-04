a014c309b429e2858d4f847be9fde601f869c235
rafaScalet <rafaelscalet2018@gmail.com>
2024-06-03 22:00:51 -0300

    book reactive forms

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Book } from '../../interfaces/book';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-books',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './books.component.css'
 })
 export class BooksComponent {
+  books: Book[] = [];
+  bookFormGroup : FormGroup;
 
+  constructor(private formBuilder: FormBuilder){
+    this.bookFormGroup = formBuilder.group({
+      id:[''],
+      title:[''],
+      synopsis:[''],
+      gender:[''],
+      year:[''],
+      rating:[''],
+      authorId:[''],
+    });
+  }
+
+  save(){
+    this.books.push(this.bookFormGroup.value);
+  }
 }
